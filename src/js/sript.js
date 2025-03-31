555e18b120ec74b482ecf04805b198028b0f2ca3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 09:08:18 -0300

    declaracoes e variaveis

---
+++ src/js/sript.js
@@ -0,0 +1,18 @@
+var nome = "fiap";
+console.log(nome);
+
+let idade = 19;
+console.log(idade); 
+
+const sobrenome = "Fiapinho";
+console.log(sobrenome);
+
+//declarando uma variavel indefinda
+let nome1;
+console.log(nome1);
+
+//declarando uma variavel vazia / nula
+let nome2=null;
+console.log(nome2);
+
+//
