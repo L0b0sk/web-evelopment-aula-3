d902cb15d9b85a2061c057ca9f815086e66bc9d2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 09:54:25 -0300

    operadores aritmedicos, logicos, comparacao

--- src/js/sript.js
+++ src/js/sript.js
@@ -1,62 +1,98 @@
-var nome = "fiap";
-console.log(nome);
+// var nome = "fiap";
+// console.log(nome);
 
-let idade = 19;
-console.log(idade); 
+// let idade = 19;
+// console.log(idade); 
 
-const sobrenome = "Fiapinho";
-console.log(sobrenome);
-
-
-//declarando uma variavel indefinda
-let nome1;
-console.log(nome1);
+// const sobrenome = "Fiapinho";
+// console.log(sobrenome);
 
 
-//declarando uma variavel vazia / nula
-let nome2=null;
-console.log(nome2);
+// //declarando uma variavel indefinda
+// let nome1;
+// console.log(nome1);
 
 
-//Tipos de variaveis 
-let exemplo1 = 10; 
-console.log(typeof exemplo1);
-
-let exemplo2 = "Tecnologia";
-console.log(typeof exemplo2);
-
-let exemplo3 = true;
-console.log(typeof exemplo3);
-
-let exemplo4 = {};
-console.log(typeof exemplo4);
-
-let exemplo5 = [];
-console.log(typeof exemplo5);
+// //declarando uma variavel vazia / nula
+// let nome2=null;
+// console.log(nome2);
 
 
-//conversoes
+// //Tipos de variaveis 
+// let exemplo1 = 10; 
+// console.log(typeof exemplo1);
 
-let numFloat = 123.456;
-console.log(parseInt(numFloat));
+// let exemplo2 = "Tecnologia";
+// console.log(typeof exemplo2);
 
-let numString = "123.456";
-console.log(parseFloat(numString));
+// let exemplo3 = true;
+// console.log(typeof exemplo3);
 
+// let exemplo4 = {};
+// console.log(typeof exemplo4);
 
-// metodos
-
-let frase = "O mundo da tecnologia";
-console.log(frase.length);
-
-
-//indexOF - retorna um trecho do texto
-
-let texto = "a progrmamacao indomavel e sustentavel";
-console.log(texto.indexOf("cao"))
+// let exemplo5 = [];
+// console.log(typeof exemplo5);
 
 
-//slice - retorna um trecho do texto com inicio e final
+// //conversoes
 
-let info = "processamento de ponta";
-console.log(info.slice(8,10));
+// let numFloat = 123.456;
+// console.log(parseInt(numFloat));
+
+// let numString = "123.456";
+// console.log(parseFloat(numString));
+
+
+// // metodos
+
+// let frase = "O mundo da tecnologia";
+// console.log(frase.length);
+
+
+// //indexOF - retorna um trecho do texto
+
+// let texto = "a progrmamacao indomavel e sustentavel";
+// console.log(texto.indexOf("cao"))
+
+
+// //slice - retorna um trecho do texto com inicio e final
+
+// let info = "processamento de ponta";
+// console.log(info.slice(8,10));
+
+
+// // operadores aritmedicos
+
+// const num1=10;
+// const num2=20;
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+
+
+// //operadores logicos
+
+// const num3=10;
+// const num4=20;
+
+// console.log(num3 < num4);
+// console.log(num3 > num4 && num4 < num3); //um e o outro 
+// console.log(num3 > num4 || num4 < num3); //ou um ou outro
+
+// // opradores de comparacao
+
+let num3=20;
+let num4="20";
+
+console.log(num3 < num4);
+console.log(num3 > num4 && num4 < num3);
+console.log(num3 < num4 || num4 < num3);
+
+
+//operadores de comparacao
+
+console.log(num3 == num4);  // compara
+console.log(num3 === num4); // compara e ve o tipo
